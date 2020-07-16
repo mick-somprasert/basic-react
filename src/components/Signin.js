3f6a40a494a863249a2bb3e07ce59f622962f240
mick-somprasert <mick_sir43@hotmail.com>
2020-07-16 10:07:39 +0700

    first commit

---
+++ src/components/Signin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Sigin extends Component {
+    render() {
+        return (
+            <div>
+                เข้าสู่ระบบ
+            </div>
+        )
+    }
+}
